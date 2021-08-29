b8c0236c830342307b2237e31b26bf6731c17abc
Padmashree Jha <codewithpom@gmail.com>
2021-08-29 22:36:29 +0530

    29/08/2021
    Final Commit of 29/08/2021

--- C/more-on-arrays/sum-pair/sum-pair.c
+++ C/more-on-arrays/sum-pair/sum-pair.c
@@ -1,8 +1,25 @@
-#include <stdio.h> // include the stdio.h library as it is the standard library in C
+#include "stdio.h" // include the stdio.h library as it is the standard library in C
 
-int pair(int arr[], int sum)
+int pair(int arr[], int sum, int size)
 {
     
+    int first_element;
+    int second_element;
+    for (int i = 0; i < size; i++)
+    {
+        first_element = arr[i];
+        for (int g = i + 1; i < size; g++)
+        {
+            second_element = arr[g];
+            if (second_element + first_element == sum)
+            {
+                int pair[] = {first_element, second_element};
+                return pair;
+            }
+        }
+    }
+    int temp[2] = {-1, NULL};
+    return temp;
 
 }
 
@@ -13,4 +30,26 @@ void print_arr(int arr[], int size) // a function just to print the arr with a s
         printf("%d ", arr[i]); // and print the element with a space
     }
     printf("\n"); // print a single line break to make the output clean for the next output
+}
+
+int main()
+{
+    int arr[] = {2, 3, 2, 4, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    print_arr(arr, size);
+    int pair_numbers[2];
+    pair_numbers = pair(arr, 7, size);
+
+    if (pair_numbers)
+    {
+        printf("%d", -1);
+
+    }
+
+    else
+    {
+        printf("%d and %d", pair_numbers[0], pair_numbers[1]);
+        
+    }
+
 }
